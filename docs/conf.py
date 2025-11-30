01bcd9b17a0669d19649140bbd7a8d0a64d47368
Physis21 <juliendudas@gmail.com>
2025-11-30 19:59:00 +0100

    fix: remove myst_parser from extensions

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,9 @@ release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# adding myst_parser causes problems
 
-extensions = ['myst_parser', 'sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
